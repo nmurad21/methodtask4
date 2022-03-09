29466a05ecee8d3a67b35a8aec5402965af26e5d
tu7kr926y <tu7kr926y@214-11>
2022-03-09 08:54:14 +0400

    Add project files.

---
+++ methodtask4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace methodtask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(CevreninSahesi(4));
+            //Console.WriteLine(DuzbucaqlininSahesi(4,5));
+           // Console.WriteLine(UcbucaqSahe(3,4,5,6));
+        }
+        #region task4-1
+        //static int CevreninSahesi(int r)
+        //  {
+        //      int s = 0;
+        //      if (r > 0)
+        //      {
+        //          int p = 3;
+        //          s = p * r * r;
+        //      }
+        //      return s;
+        //  }
+        #endregion
+        #region task4-2
+        //static int DuzbucaqlininSahesi(int a, int b)
+        //{
+        //    int s = 0;
+        //    if (a > 0 && b > 0)
+        //     {
+        //         s = a * b;
+        //     }
+        //    return s;
+        //}
+        #endregion
+        #region task4-3
+        //static double UcbucaqSahe(double r, double a, double b, double c)
+        //{
+        //    double s = 0;
+        //    double p = 0;
+        //    if(r>0 && a>0 && b>0 && c > 0)
+        //    {
+        //        p = (a + b + c) / 2;
+        //        s = p * r;
+        //    }
+        //    return s;
+        //}
+        
+        #endregion
+    }
+}
